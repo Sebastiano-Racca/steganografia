8d3429c726632ebcae597a48f40d9bf6f77130d8
Sebastiano <racca.sebastiano.7805@gmail.com>
2023-12-13 12:46:07 +0100

    Aggiunti `TODO`

--- src/Carattere.java
+++ src/Carattere.java
@@ -2,4 +2,12 @@ public class Carattere {
     Carattere(Byte[] bytes) {
         
     }
+
+    public void encode(char c) {
+        // TODO: dato un nuovo char, sostituisce gli LSB del Carattere corrente
+    }
+
+    public Byte getByteAt(int indice) {
+        // TODO: restituisce il Byte all'indice specificato
+    }
 }
--- src/Encoder.java
+++ src/Encoder.java
@@ -7,7 +7,16 @@ public class Encoder {
         this.message = message;
     }
 
+    private void writeHeader() {
+        // TODO: legge e scrive l'header nel nuovo pgm
+    }
+
+    private void writeChar(Carattere carattere) {
+        // TODO:  scrive un carattere nel nuovo pgm
+    }
+
     public void encode() {
+        // TODO: usa writeHeader() e writeChar() per scrivere ogni linea del PGM, usa Carattere.encode() per fare l'encoding di un carattere
         String[] header = this.pgm.getHeader();
         
         Byte[] bytes = new Byte[8];
