72d11480471121d803a04d00759e86cf5b7340b2
Sebastiano <racca.sebastiano.7805@gmail.com>
2023-12-14 13:12:37 +0100

    Main fatto meglio

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
 public class Main {
+    /**
+     * Entry point dell'encoder.
+     * @param args[0] Source dell'immagine PGM.
+     * @param args[1] Destinazione della nuova immagine.
+     * @param args[2] Il messaggio da nascondere.
+     */
     public static void main(String[] args) {
-        PGM pgm = new PGM("gatto.pgm", "gatto2.pgm");
-        Encoder encoder = new Encoder(pgm, "Ciao Mondo!");
+        PGM pgm = null;
+        Encoder encoder = null;
+
+        try {
+            pgm = new PGM(args[0], args[1]);
+            encoder = new Encoder(pgm, args[2]);
+
+        
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Sintassi errata:");
+            System.err.println("make run [source] [destination] [message]");
+            System.exit(1);
+        }
 
         encoder.encode();
-        
         System.out.println("Successo!");
+
     }
 }
