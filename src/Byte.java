0c5cc3ff896a74c6f6d8693787573301f2e84e8a
Sebastiano <racca.sebastiano.7805@gmail.com>
2023-12-14 13:41:05 +0100

    Aggiunti commenti

--- src/Byte.java
+++ src/Byte.java
@@ -1,16 +1,31 @@
+/**
+ * Rappresenta un pixel all'interno del PGM
+ */
 public class Byte {
     private String binario;
     private int numero;
 
+    /**
+     * Costruttore del Byte
+     * @param numero il numero
+     */
     Byte(int numero) {
         this.binario = Integer.toBinaryString(numero);
         this.numero = numero;
     }
-    
+
+    /**
+     * Restituisce l'ultimo bit significativo
+     * @return 
+     */
     public int getLSB() {
         return this.numero % 2;
     }
 
+    /**
+     * Restituisce il numero in forma decimale
+     * @return Il numero
+     */
     @Override
     public String toString() {
         return String.valueOf(this.numero);
