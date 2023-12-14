1039b08897ae61ad99145de351364b89e188b095
Tommaso Bramoso <tommaso.bramoso@istitutoagnelli.it>
2023-12-14 12:48:35 +0100

    Piccola correzione sulla visibilità del metodo "writeHeader()"

--- src/PGM.java
+++ src/PGM.java
@@ -78,7 +78,7 @@ public class PGM {
         this.writeLine(buff.toString());
     }
 
-    private void writeHeader() {
+    public void writeHeader() {
         String[] headerLines = getHeader();
         
         for (int i = 0; i < headerLines.length; i++) {
