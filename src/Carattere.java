3c7b480a5eff5d546409e435c75fef5f3d073a82
Tommaso Bramoso <tommaso.bramoso@istitutoagnelli.it>
2023-12-15 10:30:02 +0100

    Modifiche ultimo "TO DO" del metodo encode

--- src/Carattere.java
+++ src/Carattere.java
@@ -7,11 +7,23 @@ public class Carattere {
 
     public void encode(char c) {
         // TODO: dato un nuovo char, sostituisce gli LSB del Carattere corrente
+        // cicla gli 8 byte, prendi l'ultimo bit significativo e lo trasformi in base al carattere dato dall'encoder
+        String byteString = Encoder.charToByteString(c);
+
+        for (int i = 0; i < 8; i++) {
+            int lsb = byteString.charAt(i) - '0'; // Ottieni il valore del bit all'indice i
+
+            // Imposta l'ultimo bit del byte corrente
+            int nuovoLSB = lsb;
+            int numero = this.bytes[i].getNumero();
+            if (numero % 2 != nuovoLSB) {
+                numero = (numero / 2) * 2 + nuovoLSB;
+                this.bytes[i] = new Byte(numero);
+            }
+        }
+    }
     }
 
     public Byte getByteAt(int indice) {
         return bytes[indice];
     }
-}
-
-// cicla gli 8 byte, prendi l'ultimo bit significativo e lo trasformi in base al carattere dato dall'encoder
